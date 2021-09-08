584a4f90d4cc9be1087aee3ace97dc2c016886e9
Isaac Munoz <isaacmunoz1490@gmail.com>
2021-09-08 13:55:14 -0700

    Basic Authentication

--- app.js
+++ app.js
@@ -34,6 +34,35 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+// Basic authentication for a website
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+const user = auth[0];
+const pass = auth[1];
+    if ( user === 'admin' && pass === 'password') {
+      return next(); //authorized
+    } else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+app.use(auth);
+
+
+// This is where we will add the authentication, if you want for people to aces your contect you can place authentiction below the static path "public".
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
