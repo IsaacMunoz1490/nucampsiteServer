afa31413dc11f10c33b7086d04c46f83c87a8180
Isaac Munoz <isaacmunoz1490@gmail.com>
2021-09-10 09:53:30 -0700

    Express Session Part 2

--- app.js
+++ app.js
@@ -44,49 +44,31 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
-
+// Unauthenticated users can use the access
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 // Basic authentication for a website
 function auth(req, res, next) {
   console.log(req.session);
-
   if (!req.session.user) {
-      const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
-      }
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if ( user === 'admin' && pass === 'password') {
-          req.session.user = 'admin';
-          return next(); //authorized
-      } else {
-        const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
+    const err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
+  } else {
+    if (req.session.user === 'authenticated') {
+      return next();
     } else {
-      if (req.session.user === 'admin') {
-          return next();
-      } else {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-      }
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
     }
+  }
 }
 app.use(auth);
 
 
 // This is where we will add the authentication, if you want for people to aces your contect you can place authentiction below the static path "public".
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // adding our Routers
 app.use('/campsites', campsiteRouter);
